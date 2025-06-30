414e11cc3c0d03a6d80a49989cf41e205856368c
William Wsilva <williamreis.wsilva@gmail.com>
2025-06-30 14:04:28 -0300

    Edit: editando arquivo create databases

--- src/configs/create databases.py
+++ src/configs/create databases.py
@@ -3,28 +3,42 @@
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- DROP DATABASE adventure_works_bronze CASCADE; 
-# MAGIC
-# MAGIC CREATE DATABASE IF NOT EXISTS adventure_works_bronze
-# MAGIC LOCATION '/mnt/adlsadventureworksprd/bronze/adventure_works_bronze'
+# ADLS
+storage_acount_name = 'wsadlsadwprd'
+
+# Lista conteúdo se o mount funcionar
+try:
+    display(dbutils.fs.ls(f"/mnt/{storage_acount_name}/adw/"))
+except Exception as e:
+    print("Erro ao listar o conteúdo:", e)
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- DROP DATABASE adventure_works_prata CASCADE; 
-# MAGIC
-# MAGIC CREATE DATABASE IF NOT EXISTS adventure_works_prata
-# MAGIC LOCATION '/mnt/adlsadventureworksprd/prata/adventure_works_prata'
+%sql
+-- ATENÇÃO!!! DELETAR BANCO DE DADOS ???
+USE CATALOG hive_metastore;
+DROP DATABASE IF EXISTS adventure_works_silver CASCADE
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- DROP DATABASE adventure_works_ouro CASCADE; 
-# MAGIC
-# MAGIC CREATE DATABASE IF NOT EXISTS adventure_works_ouro
-# MAGIC LOCATION '/mnt/adlsadventureworksprd/ouro/adventure_works_prata'
+%sql
+USE CATALOG hive_metastore;
+
+CREATE DATABASE IF NOT EXISTS adventure_works_bronze
+LOCATION '/mnt/wsadlsadwprd/adw/001_bronze/adventure_works_bronze'
 
 # COMMAND ----------
 
+%sql
+USE CATALOG hive_metastore;
 
+CREATE DATABASE IF NOT EXISTS adventure_works_prata
+LOCATION '/mnt/wsadlsadwprd/adw/002_silver/adventure_works_prata'
+
+# COMMAND ----------
+
+%sql
+USE CATALOG hive_metastore;
+
+CREATE DATABASE IF NOT EXISTS adventure_works_ouro
+LOCATION '/mnt/wsadlsadwprd/adw/003_gold/adventure_works_ouro'
