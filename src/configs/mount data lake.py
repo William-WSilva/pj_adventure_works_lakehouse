0def31a6cd96861fcd27330195558a1ba7e62baf
William Wsilva <williamreis.wsilva@gmail.com>
2025-06-30 13:57:04 -0300

    Edit: mount data lake

--- src/configs/mount data lake.py
+++ src/configs/mount data lake.py
@@ -1,95 +1,53 @@
 # Databricks notebook source
-# ADLS
-storage_account_name = "adlsadventureworksprd" 
-
-# APP Registration
-client_id = dbutils.secrets.get('adventure-works', 'client-id')
-tenant_id = dbutils.secrets.get('adventure-works', 'tenant-id')
-client_secret = dbutils.secrets.get('adventure-works', 'client-secret')
+# obter lista de segredos Azure
+dbutils.secrets.list('ws_adb_secret_scope')
 
 # COMMAND ----------
 
-configs = {"fs.azure.account.auth.type": "OAuth",
-          "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-          "fs.azure.account.oauth2.client.id": f"{client_id}",
-          "fs.azure.account.oauth2.client.secret": f"{client_secret}",
-          "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
+# ADLS
+storage_acount_name = 'wsadlsadwprd'
+
+# App Registration
+client_id           = dbutils.secrets.get('ws_adb_secret_scope', 'client-id')
+tenant_id           = dbutils.secrets.get('ws_adb_secret_scope', 'tenant-id')
+client_secret       = dbutils.secrets.get('ws_adb_secret_scope', 'client-secret')
+
+# COMMAND ----------
+
+configs = {
+    "fs.azure.account.auth.type": "OAuth",
+    "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+    "fs.azure.account.oauth2.client.id": client_id,
+    "fs.azure.account.oauth2.client.secret": client_secret,
+    "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
+}
 
 # COMMAND ----------
 
 def mount_adls(container_name):
-    dbutils.fs.mount(
-      source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
-      mount_point = f"/mnt/{storage_account_name}/{container_name}",
-    extra_configs = configs)
+    mount_point = f"/mnt/{storage_acount_name}/{container_name}"
+
+    # Verificar se o ponto já está montado
+    montar_mount_point = True
+    for i in dbutils.fs.mounts():
+        if i.mountPoint == mount_point:
+            montar_mount_point = False
+            break
+
+    # Se não estiver montado, faz o mount
+    if  montar_mount_point:
+        dbutils.fs.mount(
+            source=f"abfss://{container_name}@{storage_acount_name}.dfs.core.windows.net/",
+            mount_point=mount_point,
+            extra_configs=configs)
+    else:
+        print(f"O container já está montado: {mount_point}")
 
 # COMMAND ----------
 
-mount_adls('landing-zone')
-
-# COMMAND ----------
-
-mount_adls('prata')
-
-# COMMAND ----------
-
-display(dbutils.fs.mounts())
-
-# COMMAND ----------
-
-dbutils.fs.unmount('/mnt/adlsadventureworksprd/prata')
-
-# COMMAND ----------
-
-mount_adls('controle')
-
-# COMMAND ----------
-
-mount_adls('bronze')
-
-# COMMAND ----------
-
-mount_adls('prata')
-
-# COMMAND ----------
-
-mount_adls('ouro')
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/mnt/adlsadventureworksprd/controle'))
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/mnt/adlsadventureworksprd/bronze'))
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/mnt/adlsadventureworksprd/prata'))
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/mnt/adlsadventureworksprd/ouro'))
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/mnt/adlsadventureworksprd/landing-zone/AdventureWorks/Full_load/Production'))
-
-# COMMAND ----------
-
-df = spark.read.parquet('/mnt/adlsadventureworksprd/landing-zone/AdventureWorks/Full_load/Production/Production_BillOfMaterials.parquet')
-
-# COMMAND ----------
-
-df.display()
-
-# COMMAND ----------
-
-df.write.format('delta').save('/mnt/adlsadventureworksprd/bronze/primeira_tabela')
-
-# COMMAND ----------
+# Lista conteúdo se o mount funcionar
+try:
+    display(dbutils.fs.ls(f"/mnt/{storage_acount_name}/adw/"))
+except Exception as e:
+    print("Erro ao listar o conteúdo:", e)
 
